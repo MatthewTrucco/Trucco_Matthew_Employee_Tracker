f17da147e7f78af2cce7c0dc95504871f6dadb53
MatthewTrucco <87626582+MatthewTrucco@users.noreply.github.com>
2023-09-21 15:48:11 -0400

    Update server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const cfonts = require('cfonts');
+
+// create a MySQL connection
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employeeTracker_db",
+});
+
+// connect to the database
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to the database!");
+    // start the application
+    start();
+});
